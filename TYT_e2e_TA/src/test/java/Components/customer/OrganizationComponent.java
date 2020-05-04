7e6bdc86fb0353704c304d186b660d11d3c4f47d
unknown <tfred@live.de>
2020-05-04 22:16:10 +0100

    customer

--- TYT_e2e_TA/src/test/java/Components/customer/OrganizationComponent.java
+++ TYT_e2e_TA/src/test/java/Components/customer/OrganizationComponent.java
@@ -1,48 +1,85 @@
 package Components.customer;
 
 import Pages.ReusableViewElements;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OrganizationComponent extends ReusableViewElements {
-    private final By streetName = By.id("prospectdetail:prospectView:main:primaryAddress:address");
-    private final By streetNumber = By.id("prospectdetail:prospectView:main:primaryAddress:houseNumber");
-    private final By zipCode = By.id("prospectdetail:prospectView:main:primaryAddress:postalCode");
-    private final By phoneNumber = By.id ("prospectdetail:prospectView:main:primaryAddress:houseNumber");
-    private final By city = By.id("prospectdetail:prospectView:main:primaryAddress:city");
-    private final By country = By.id ("prospectdetail:prospectView:main:primaryAddress:country_label");
+    private final By streetName = By.id("prospectdetail:prospectView:main:bpPrimaryAddress:address");
+    private final By streetNumber = By.id("prospectdetail:prospectView:main:bpPrimaryAddress:houseNumber");
+    private final By zipCode = By.id("prospectdetail:prospectView:main:bpPrimaryAddress:postalCode");
+    private final By phoneNumber = By.id ("prospectdetail:prospectView:main:bpPrimaryAddress:phoneNrs:0:typedPhone_localNr");
+    private final By city = By.id("prospectdetail:prospectView:main:bpPrimaryAddress:city");
+    private final By country = By.id ("prospectdetail:prospectView:main:bpPrimaryAddress:country_label");
     private final By company = By.id("prospectdetail:prospectView:main:primaryAddress:name");
+    private final String legaly_entity = "prospectdetail:prospectView:main:legalentity_";
+    private final By saveBtn = By.id("prospectdetail:saveButton");
 
     public OrganizationComponent(WebDriver driver) {
         super(driver);
     }
 
+    public void setLegaly_entity(String legal_entity) {
+        Select dropdown = new Select(getDriver().
+                findElement(By.className("ui-selectonemenu-trigger")));
+        dropdown.selectByVisibleText(legal_entity);
+        //element(By.id(legaly_entity+position)).click();
+    }
+
+    public void verifiedSelectedEntity (String name, int position){
+        WebElement elt = element(By.id(legaly_entity+position));
+        Assert.assertEquals("there are not equals", elt.getText(), name);
+    }
+
+
     public void setCompany(String company_Name){
         element(company).sendKeys(company_Name);
+       JavascriptExecutor jse = (JavascriptExecutor)getDriver();
+        jse.executeScript("scroll(0, 300);");
     }
 
     public void setStreetName(String street_Name){
+       //scrollToElement(element(streetName));
         element(streetName).sendKeys(street_Name);
+        waitABit(2000);
     }
 
     public void setStreetNumber(String number){
+
         element(streetNumber).sendKeys(number);
+        waitABit(2000);
     }
 
-    public void setZipCode(String code){
-        element(zipCode).sendKeys(code);
+    public void setZipCode(String zip_code){
+        element(zipCode).sendKeys(zip_code);
+        waitABit(2000);
     }
 
     public void setCity (String theCity){
         element(city).sendKeys(theCity);
+        waitABit(2000);
     }
 
     public void setPhoneNumber(String phone_Number){
+
         element(phoneNumber).sendKeys(phone_Number);
+        waitABit(2000);
     }
 
     public void setCountry(String theCountry){
+
         element(country).sendKeys(theCountry);
+        waitABit(2000);
     }
 
+    public void save() {
+        element(saveBtn).click();
+    }
 }
--- TYT_e2e_TA/src/test/java/e2eTests/cukeStepDefinitions/milesWeb/customer/CustomerPageCucumberSteps.java
+++ TYT_e2e_TA/src/test/java/e2eTests/cukeStepDefinitions/milesWeb/customer/CustomerPageCucumberSteps.java
@@ -3,12 +3,12 @@ package e2eTests.cukeStepDefinitions.milesWeb.customer;
 import Pages.customer.CustomerPage;
 import e2eTests.stepdefinitions.CustomerPageSteps;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.thucydides.core.annotations.Step;
 import net.thucydides.core.annotations.Steps;
 
 public class CustomerPageCucumberSteps {
+
+    CustomerPage customerPage;
 
     @Steps
     public CustomerPageSteps customerPageSteps;
@@ -25,31 +25,46 @@ public class CustomerPageCucumberSteps {
 
     @And("he gives the street name {string} in the contact data form")
     public void heGivesTheStreetNameStreet_nameInTheContactDataForm(String streetName) {
-//        customerPage.getOrganizationComponent().setStreetName(streetName);
+        customerPage.getOrganizationComponent().setStreetName(streetName);
     }
 
     @And("he gives the street number {string} in the contact data form")
     public void heGivesTheStreetNumberStreet_numberInTheContactDataForm(String street_number) {
-//        customerPage.getOrganizationComponent().setStreetNumber(street_number);
+        customerPage.getOrganizationComponent().setStreetNumber(street_number);
     }
 
     @And("he gives the city name {string} in the contact data form")
     public void heGivesTheCityNameCity_nameInTheContactDataForm(String city) {
-//        customerPage.getOrganizationComponent().setCity(city);
+        customerPage.getOrganizationComponent().setCity(city);
     }
 
     @And("he gives the zip code {string} in the contact data form")
     public void heGivesTheZipCodeZip_codeInTheContactDataForm(String zipCode) {
-//        customerPage.getOrganizationComponent().setZipCode(zipCode);
+        customerPage.getOrganizationComponent().setZipCode(zipCode);
     }
 
     @And("he select the country {string} in the contact data form")
     public void heSelectTheCountryCountryInTheContactDataForm(String country) {
-//        customerPage.getOrganizationComponent().setCountry(country);
+        customerPage.getOrganizationComponent().setCountry(country);
     }
 
     @And("he gives the phone number {string} in the contact data form")
     public void heGivesThePhoneNumberPhone_numberInTheContactDataForm(String phoneNumber) {
-//        customerPage.getOrganizationComponent().setPhoneNumber(phoneNumber);
+        customerPage.getOrganizationComponent().setPhoneNumber(phoneNumber);
+    }
+
+    @And("he select the legal entity {string}")
+    public void heSelectTheLegalEntityLegal_entity(String legalEntity) {
+        customerPage.getOrganizationComponent().setLegaly_entity(legalEntity);
+    }
+
+    @And("he click on the button save")
+    public void heClickOnTheButtonSave() {
+        customerPage.getOrganizationComponent().save();
+    }
+
+    @And("he choose the legal entity {string}")
+    public void heChooseTheLegalEntityLegal(String legalEntity) {
+        customerPage.getOrganizationComponent().setLegaly_entity(legalEntity);
     }
 }
