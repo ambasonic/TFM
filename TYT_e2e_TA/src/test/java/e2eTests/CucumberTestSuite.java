73adb19676925b7a9c31df52890c4c16afc95b0f
Neg Marron <tfred@live.de>
2020-08-06 17:04:38 +0200

    price update

--- TYT_e2e_TA/src/test/java/e2eTests/CucumberTestSuite.java
+++ TYT_e2e_TA/src/test/java/e2eTests/CucumberTestSuite.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "src/test/resources/features",
-        tags = "@pck7case15"
+        tags = "@pck7case14"
 )
 public class CucumberTestSuite {
 
